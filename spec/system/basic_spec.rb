1cd5e05fbedfae3d1024ae9744c66714141fe6ae
Justin Clayton <justin@claytons.net>
2013-08-25 19:20:38 -0700

    added rest of the redis tests

--- spec/system/basic_spec.rb
+++ spec/system/basic_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper_system'
 
-describe 'basic tests:' do
+describe 'testing module installation:' do
 
   pp = <<-EOS
     class { 'redis': enable_epel => true, }
@@ -8,7 +8,33 @@ describe 'basic tests:' do
 
   # Using puppet_apply as a subject
   context puppet_apply(pp) do
-    its(:stderr) { should be_empty }
+    its(:stderr)    { should be_empty }
     its(:exit_code) { should_not == 1 }
+  end
+
+end
+
+describe 'testing redis itself:' do
+
+  context shell 'redis-cli ping' do
+    its(:stdout)    { should =~ /PONG/ }
+    its(:stderr)    { should be_empty  }
+    its(:exit_code) { should be_zero   }
+  end
+
+  context shell 'redis-cli set abc "Hello World"' do
+    its(:stderr)    { should be_empty }
+    its(:exit_code) { should be_zero  }
+  end
+
+  context shell 'redis-cli get abc' do
+    its(:stdout)    { should =~ /Hello World/ }
+    its(:stderr)    { should be_empty         }
+    its(:exit_code) { should be_zero          }
+  end
+
+  context shell 'redis-cli del abc' do
+    its(:stderr)    { should be_empty }
+    its(:exit_code) { should be_zero  }
   end
 end
